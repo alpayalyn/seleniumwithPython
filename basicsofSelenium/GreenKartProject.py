f6f995d9452de61f66e8d1b525a0d98691ce3a41
alpayalyn <alpay.alin@useinsider.com>
2021-11-27 12:27:53 +0300

    LastVersion.LCW

--- basicsofSelenium/GreenKartProject.py
+++ basicsofSelenium/GreenKartProject.py
@@ -1,26 +1,39 @@
 from selenium import webdriver
+import unittest
+from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 
-class GreenKart:
+
+class TestRun(unittest.TestCase):
     WEBSITE = "https://rahulshettyacademy.com/seleniumPractise/#/"
 
-    def __init__(self):
+    def setUp(self):
         self.driver = webdriver.Chrome("C:/seleniumdriver/chromedriver")
         self.driver.maximize_window()
         self.driver.get(self.WEBSITE)
-        self.wait = WebDriverWait(self.driver, 15)
+        self.wait = WebDriverWait(self.driver, 3)
 
-    def test(self):
+    def greenKart(self):
         self.driver.find_element_by_css_selector("input.search-keyword").send_keys("ber")
         count = len(self.driver.find_elements_by_xpath("//button[@type='button']"))
         assert count == 3 
-        butons = self.driver.find_elements_by_xpath("//div[@class='product-action']/button") #
+        butons = self.driver.find_elements_by_xpath("//div[@class='product-action']/button") # We are assigning all of locators. and there are 3 same button elements. so it will create 3 elemnts as array
 
         for buton in butons:
             buton.click()
         
         self.driver.find_elements_by_xpath("img[alt='Cart']").click()
         self.driver.find_elements_by_xpath("//button[text()='PROCEED TO CHECKOUT']").click()
+        wait = WebDriverWait(self.driver, 5)
+        assert self.wait.until(ec.presence_of_element_located(By.CLASS_NAME, "promoCode"))
+        self.driver.find_element_by_class_name("promoCode").send_key("rahulshettyacademy")
+        self.driver.find_element_by_css_selector("button[class='promoBtn']").click()
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
--- basicsofSelenium/lcwaikikiSecond.py
+++ basicsofSelenium/lcwaikikiSecond.py
@@ -1,37 +1,59 @@
 from selenium import webdriver
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common import by
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-
+import time
 
 class TestRun(unittest.TestCase):
     WEBSITE = "https://www.lcwaikiki.com/tr-TR/TR"
     CATEGORY_PAGE_1 = (By.CSS_SELECTOR, '.sf-with-ul')
     PRODUCT_PAGE = (By.CSS_SELECTOR, '.product-item-wrapper')
     ADD_TO_CART = (By.CSS_SELECTOR, '.col-xl-12')
-    CART_PAGE = (By.CSS_SELECTOR, '.header-cart')
+    CART_PAGE = (By.CSS_SELECTOR, '.bndl-shopping-bag')
     MAIN_PAGE = (By.CSS_SELECTOR, '.header-logo.img-logo')
     HEADER_CONTAINER = (By.CSS_SELECTOR, '.div-header-container')  # check for first page
-    MOST_SOLD = (By.CSS_SELECTOR, '.uzun.visible-lg.visible-md')  # check for second page
-    ADD_TO_CART_CHECK = (By.CSS_SELECTOR, '.col-xs-12"')  # check for add to cart
-    # MEN_BREADCRUMB = (By.xp, "//a[@href='/tr-TR/TR/urun-grubu/erkek']") # $x("//a[@href='/tr-TR/TR/urun-grubu/erkek']")[2]
-    CART_PAGE_CHECK = (By.CSS_SELECTOR, '.cart-square-link')
+    MEN_BREADCRUMB = (By.XPATH, "//span[@itemprop='name']")
+    SIZE_SECTION = (By.XPATH, "//a[@data-tracking-label='BedenTablosu']")
+    SIZE_CHOOSE = (By.XPATH, "//a[@size='M']")
+    PAYMENT_PROCESS_CHECK = (By.XPATH, "//a[@data-tracking-label='Siparişi Tamamla']")
 
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path="C:/seleniumdriver/chromedriver")
         self.driver.maximize_window()
         self.driver.get(self.WEBSITE)
-        self.wait = WebDriverWait(self.driver, 3)
+        self.wait = WebDriverWait(self.driver, 10)
 
     def test_LCW(self):
+        assert self.wait.until(ec.presence_of_all_elements_located(self.HEADER_CONTAINER))[0].is_displayed()
         self.wait.until(ec.presence_of_all_elements_located(self.CATEGORY_PAGE_1))[1].click()
-        self.wait.until(ec.presence_of_all_elements_located(self.PRODUCT_PAGE))[1].click()  # click the item
-        self.wait.until(ec.presence_of_all_elements_located("1"))[0].click()
-        self.wait.until(ec.presence_of_all_elements_located(self.CART_PAGE))[0].click()
-        self.wait.until(ec.presence_of_all_elements_located(self.MAIN_PAGE))[0].click()
 
+        time.sleep(2)
+        erkekText = self.wait.until(ec.presence_of_all_elements_located(self.MEN_BREADCRUMB))[0].text
+        assert erkekText == "Erkek", "This page doesnt contain Erkek BreadCrumb"
+        self.wait.until(ec.presence_of_all_elements_located(self.PRODUCT_PAGE))[1].click() 
+
+        time.sleep(2)
+        bedenTablosu = self.wait.until(ec.presence_of_all_elements_located(self.SIZE_SECTION))[0].text
+        assert bedenTablosu == 'Beden Tablosu', "This page doesnt contain Beden Tablosu"
+
+        time.sleep(2)
+        self.wait.until(ec.presence_of_all_elements_located(self.SIZE_CHOOSE))[0].click()
+
+        time.sleep(2)
+        self.wait.until(ec.presence_of_all_elements_located(self.ADD_TO_CART))[0].click()  
+
+        time.sleep(2)
+        self.wait.until(ec.presence_of_all_elements_located(self.CART_PAGE))[0].click()
+        odemeAdimi = self.wait.until(ec.presence_of_all_elements_located(self.PAYMENT_PROCESS_CHECK))[0].text
+        assert odemeAdimi == "ÖDEME ADIMINA GEÇ", "This page doesnt contain Proceed with Payment Button"
+
+        time.sleep(2)     
+        self.wait.until(ec.presence_of_all_elements_located(self.MAIN_PAGE))[0].click()
+        assert self.wait.until(ec.presence_of_element_located(self.HEADER_CONTAINER)).is_displayed(), "You are not on the HomePage"
+        time.sleep(2)     
 
 if __name__ == "__main__":
     unittest.main()
