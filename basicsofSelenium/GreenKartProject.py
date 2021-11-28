d3425614c349bfcb911b3447ccb73c5f6aba4e51
alpayalyn <alpay.alin@useinsider.com>
2021-11-28 15:15:33 +0300

    ComparisonOfItemsInCartPage&CheckoutPage

--- basicsofSelenium/GreenKartProject.py
+++ basicsofSelenium/GreenKartProject.py
@@ -8,6 +8,8 @@ from selenium.webdriver.common.by import By
 
 class TestRun(unittest.TestCase):
     WEBSITE = "https://rahulshettyacademy.com/seleniumPractise/#/"
+    listbut = []
+    listveg = []
 
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path="C:/seleniumdriver/chromedriver")
@@ -27,13 +29,25 @@ class TestRun(unittest.TestCase):
         for buton in butons:
             time.sleep(3)
             buton.click()
+            self.listbut.append(buton.find_element_by_xpath("parent::div/parent::div/h4").text) # this will help to get what is written in buton element. the whole function is: ("//div[@class='product-action']/button/parent::div/parent::div/h4")
+        print(self.listbut)
 
         time.sleep(3)
         self.driver.find_element_by_css_selector("img[alt='Cart']").click()
         time.sleep(3)
         self.driver.find_element_by_xpath("//button[text()='PROCEED TO CHECKOUT']").click()
         time.sleep(3)
-        self.driver.find_element_by_class_name("promoCode").send_key("rahulshettyacademy")
+        
+        veggies =  self.driver.find_elements_by_css_selector("p.product-name") # critical way to compare texts of items in cart page and payment page
+        for veg in veggies:
+            time.sleep(3)
+            self.listveg.append(veg.text)
+        print(self.listveg)
+
+        assert self.listbut == self.listveg, "Items were bought are not same"
+
+        time.sleep(3)
+        self.driver.find_element_by_class_name("promoCode").send_keys("rahulshettyacademy")
         time.sleep(3)
         self.driver.find_element_by_css_selector("button[class='promoBtn']").click()
 
